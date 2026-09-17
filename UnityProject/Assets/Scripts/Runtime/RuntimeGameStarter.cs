62172e505cc3dc8565b2e441342f3e4d1e277b7d
dhavalwork97-jpg <dhaval.work97@gmail.com>
2026-09-17 13:55:24 +0530

    feat: auto-start Unity prototype world

---
+++ UnityProject/Assets/Scripts/Runtime/RuntimeGameStarter.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace UltimateTruckEmpire
+{
+    public static class RuntimeGameStarter
+    {
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
+        private static void StartGame()
+        {
+            if (Object.FindFirstObjectByType<PrototypeWorldBuilder>() != null)
+                return;
+
+            var root = new GameObject("UltimateTruckEmpireRuntime");
+            root.AddComponent<TycoonStateService>();
+            root.AddComponent<GameBootstrap>();
+            root.AddComponent<PrototypeWorldBuilder>();
+        }
+    }
+}
