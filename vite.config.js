0255a66a2a62c5f9c4524cda74e2ebf5f4fad92a
dhavalwork97-jpg <dhaval.work97@gmail.com>
2026-09-17 11:01:08 +0530

    fix: use root Vercel base path

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/ultimate-truck-empire-3d/',
+  base: '/',
   plugins: [react()],
 });
