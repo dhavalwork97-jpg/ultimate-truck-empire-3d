4a87c57921a31f11bfe7017ada167ea1e9690e14
dhavalwork97-jpg <dhaval.work97@gmail.com>
2026-09-22 12:39:14 +0530

    Integrate modular cargo loading into LoadedTrailer

--- Assets/Scripts/TrailerSystem/Runtime/LoadedTrailer.cs
+++ Assets/Scripts/TrailerSystem/Runtime/LoadedTrailer.cs
@@ -8,6 +8,7 @@ namespace UltimateTruckEmpire.TrailerSystem
         [SerializeField] private CargoDefinition cargo;
         [SerializeField] private TrailerSkinDefinition skin;
         [SerializeField] private float cargoWeightTons;
+        [SerializeField] private TrailerCargoModule cargoModule;
 
         public TrailerDefinition Trailer => trailer;
         public CargoDefinition Cargo => cargo;
@@ -26,6 +27,15 @@ namespace UltimateTruckEmpire.TrailerSystem
             cargoWeightTons = TrailerCompatibility.GetAllowedWeight(trailer, cargo, weightTons);
             skin = skinDefinition != null ? skinDefinition : trailer.defaultSkin;
 
+            cargoModule = GetComponent<TrailerCargoModule>() ?? gameObject.AddComponent<TrailerCargoModule>();
+            cargoModule.Initialize(trailer);
+            if (!cargoModule.Load(cargo))
+            {
+                cargo = null;
+                cargoWeightTons = 0f;
+                return false;
+            }
+
             var applier = GetComponent<TrailerSkinApplier>() ?? gameObject.AddComponent<TrailerSkinApplier>();
             applier.Initialize(trailer, skin);
             return true;
@@ -33,6 +43,8 @@ namespace UltimateTruckEmpire.TrailerSystem
 
         public void Unload()
         {
+            if (cargoModule != null)
+                cargoModule.Unload();
             cargo = null;
             cargoWeightTons = 0f;
         }
