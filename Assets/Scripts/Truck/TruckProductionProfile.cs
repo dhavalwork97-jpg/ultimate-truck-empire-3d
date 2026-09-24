416e64017dcd8973a251ffde25099e9da0f51772
dhavalwork97-jpg <dhaval.work97@gmail.com>
2026-09-24 14:37:11 +0530

    Prepare truck production profile contract

---
+++ Assets/Scripts/Truck/TruckProductionProfile.cs
@@ -0,0 +1,26 @@
+#if UNITY_EDITOR
+using UnityEngine;
+
+namespace UltimateTruckEmpire.Truck
+{
+    [CreateAssetMenu(fileName = "TruckProductionProfile", menuName = "Ultimate Truck Empire/Truck/Production Profile")]
+    public sealed class TruckProductionProfile : ScriptableObject
+    {
+        public string id = "truck-id";
+        public string displayName = "New Truck";
+        public string manufacturer = "UTE Motors";
+        public GameObject prefab;
+        public float emptyMassTons = 8f;
+        public float fuelCapacityLitres = 500f;
+        public float enginePowerHp = 320f;
+        public float maxSpeedKph = 110f;
+        public float fuelEfficiency = 7.2f;
+        public string drivetrain = "6x4";
+        public Transform couplingSocket;
+        public Transform[] wheelSockets;
+        public int lodCount = 1;
+        public int maxTextureResolution = 1024;
+        public int materialSlotBudget = 6;
+    }
+}
+#endif
