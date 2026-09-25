4f1e6d636feb2282e15826f15cfce5e13ee02999
dhavalwork97-jpg <dhaval.work97@gmail.com>
2026-09-25 09:37:41 +0530

    fix: import master freight world map namespace

--- Assets/Scripts/FreightSystem/FreightMarketService.cs
+++ Assets/Scripts/FreightSystem/FreightMarketService.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UltimateTruckEmpire.World;
 using UltimateTruckEmpire.Economy;
 using UltimateTruckEmpire.Save;
 using UltimateTruckEmpire.Truck;
