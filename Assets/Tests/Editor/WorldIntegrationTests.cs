5a05c1890f81de25ead5b27a7d860d2d003d8626
dhavalwork97-jpg <dhaval.work97@gmail.com>
2026-09-25 19:05:24 +0530

    test: count freight delivery triggers explicitly

--- Assets/Tests/Editor/WorldIntegrationTests.cs
+++ Assets/Tests/Editor/WorldIntegrationTests.cs
@@ -85,28 +85,31 @@ namespace UltimateTruckEmpire.Tests.Editor
                 UltimateTruckEmpire.World.FreightWorldBuilder.BuildZones(freightRoot.transform);
 
                 var roadColliders = overlayGo.GetComponentsInChildren<MeshCollider>();
-                var triggers = freightRoot.GetComponentsInChildren<BoxCollider>();
+                var deliveryTriggers = freightRoot.GetComponentsInChildren<UltimateTruckEmpire.Gameplay.DeliveryTrigger>();
 
                 Assert.That(roadColliders.Length, Is.GreaterThan(0));
-                Assert.That(triggers.Length, Is.EqualTo(UltimateTruckEmpire.World.FreightWorldMap.Cities.Count * 2));
+                Assert.That(deliveryTriggers.Length, Is.EqualTo(UltimateTruckEmpire.World.FreightWorldMap.Cities.Count * 2));
 
-                for (int i = 0; i < triggers.Length; i++)
+                for (int i = 0; i < deliveryTriggers.Length; i++)
                 {
+                    var trigger = deliveryTriggers[i].GetComponent<BoxCollider>();
+                    Assert.That(trigger, Is.Not.Null);
+
                     for (int j = 0; j < roadColliders.Length; j++)
                     {
                         bool overlaps = Physics.ComputePenetration(
                             roadColliders[j],
                             roadColliders[j].transform.position,
                             roadColliders[j].transform.rotation,
-                            triggers[i],
-                            triggers[i].transform.position,
-                            triggers[i].transform.rotation,
+                            trigger,
+                            trigger.transform.position,
+                            trigger.transform.rotation,
                             out _,
                             out _);
 
                         Assert.IsFalse(
                             overlaps,
-                            triggers[i].transform.parent.name + " overlaps " + roadColliders[j].gameObject.name);
+                            trigger.transform.parent.name + " overlaps " + roadColliders[j].gameObject.name);
                     }
                 }
             }
