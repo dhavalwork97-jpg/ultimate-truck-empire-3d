35909369f330cb3928fec3e5c454251db910d626
dhavalwork97-jpg <dhaval.work97@gmail.com>
2026-09-26 18:38:44 +0530

    test: validate freight prefab and company contracts

--- Assets/Tests/Editor/FreightLocationTests.cs
+++ Assets/Tests/Editor/FreightLocationTests.cs
@@ -69,6 +69,76 @@ namespace UltimateTruckEmpire.Tests.Editor
 
             Assert.That(missing, Is.Empty, string.Join("\n", missing));
         }
+
+
+#if UNITY_EDITOR
+        [Test]
+        public void PrefabContractsContainAnchorsAndThreeLodLevels()
+        {
+            string[] prefabNames =
+            {
+                "Warehouse_Base", "Warehouse_Large", "Warehouse_Port",
+                "Factory_Base", "Factory_Textile", "Factory_Chemical", "Factory_Automotive"
+            };
+            string[] anchors =
+            {
+                "MeshRoot", "LoadingDock_A", "LoadingDock_B", "TrailerSpawn",
+                "CargoSpawn", "DeliveryTrigger", "ParkingSlot_01", "ParkingSlot_02",
+                "CompanySign", "EnvironmentCollision"
+            };
+
+            foreach (var prefabName in prefabNames)
+            {
+                string folder = prefabName.StartsWith("Warehouse") ? "Warehouse" : "Factory";
+                string path = $"Assets/FreightLocations/Prefabs/{folder}/{prefabName}.prefab";
+                var root = PrefabUtility.LoadPrefabContents(path);
+                try
+                {
+                    Assert.That(root, Is.Not.Null, path);
+                    foreach (var anchor in anchors)
+                        Assert.That(FindChild(root.transform, anchor), Is.Not.Null, $"{prefabName} missing {anchor}");
+
+                    var lod = root.GetComponent<LODGroup>();
+                    Assert.That(lod, Is.Not.Null, $"{prefabName} missing LODGroup");
+                    Assert.That(lod.lodCount, Is.EqualTo(3), $"{prefabName} must have 3 LOD levels");
+
+                    var delivery = FindChild(root.transform, "DeliveryTrigger");
+                    Assert.That(delivery.gameObject.isStatic, Is.False, $"{prefabName} DeliveryTrigger must be non-static");
+                }
+                finally
+                {
+                    PrefabUtility.UnloadPrefabContents(root);
+                }
+            }
+        }
+
+        [Test]
+        public void ResourcePrefabCatalogContainsEveryRegisteredLocation()
+        {
+            var catalog = Resources.Load<FreightLocationPrefabCatalog>("FreightLocations/FreightLocationPrefabCatalog");
+            Assert.That(catalog, Is.Not.Null);
+            foreach (var location in FreightLocationRegistry.All)
+                Assert.That(catalog.Find(location.id), Is.Not.Null, location.id);
+        }
+
+        [Test]
+        public void ExampleCompanyDefinitionsExist()
+        {
+            string folder = "Assets/FreightLocations/Data/Companies";
+            string[] assets = AssetDatabase.FindAssets("t:FreightLocationCompanyDefinition", new[] { folder });
+            Assert.That(assets.Length, Is.GreaterThanOrEqualTo(6));
+        }
+
+        private static Transform FindChild(Transform root, string name)
+        {
+            if (root.name == name) return root;
+            for (int i = 0; i < root.childCount; i++)
+            {
+                var found = FindChild(root.GetChild(i), name);
+                if (found != null) return found;
+            }
+            return null;
+        }
 #endif
     }
 }
